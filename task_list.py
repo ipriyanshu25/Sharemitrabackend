259193539a3ada51889063cf4467bfa2f6da48c6
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2025-03-21 16:12:59 +0530

    Update task_list.py

--- task_list.py
+++ task_list.py
@@ -1,6 +1,6 @@
 from flask import Flask,request, jsonify , Blueprint
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime,timedelta
 from bson.objectid import ObjectId
 import re
 
@@ -19,36 +19,49 @@ URL_REGEX = re.compile(
     '*)?$'  # port and path
 )
 
-task_list_bp.route("/display-message", methods=['POST'])
+@task_list_bp.route("/display-message", methods=['POST'])
 def display_message():
     data = request.get_json()
+    title = data.get("title")
     message = data.get("message")
+    task_link = data.get("link")
     if not message:
         return jsonify({"status": 0, "msg": "Message cannot be empty!", "class": "error"})
+    if not task_link or not re.match(URL_REGEX, task_link):
+        return jsonify({"status": 0, "msg": "Invalid URL format", "class": "error"})
+
     new_task ={
+        "title" : title,
         "message" : message,
+        "link" : task_link,
         "created_at":datetime.utcnow()
     }
     result = tasks_collection.insert_one(new_task)
     if result.inserted_id:
-        return jsonify({"status": 1, "msg": "Task message added succefylly", "class": "success"})
+        return jsonify({"status": 200, "msg": "Task message added succefylly"})
     else:
-        return jsonify({"status": 0, "msg":"Failed to add Task", "class": "error"})
+        return jsonify({"status": 404, "msg":"Failed to add Task"})
 
 
 
 
-
-task_list_bp.route("/tasks", methods=['GET'])
+@task_list_bp.route("/tasks", methods=['GET'])
 def get_tasks():
-    """Retrieve tasks in reverse order (newest first)"""
-    tasks = list(tasks_collection.find().sort("created_at", -1))  
-
+    """Retrieve tasks in reverse order (newest first) and mark new tasks (within last 72 hours)"""
+    tasks = list(tasks_collection.find().sort("created_at", -1))
+    
+    # Calculate the threshold datetime (72 hours ago)
+    now = datetime.utcnow()
+    threshold = now - timedelta(hours=72)
+    
     task_list = [
         {
-            "id": str(task["_id"]),
+            "id": str(task["_id"]),  # Convert ObjectId to string
+            "title": task["title"],
             "message": task["message"],
-            "created_at": task["created_at"].strftime("%Y-%m-%d %H:%M:%S")
+            "link": task["link"],
+            "created_at": task["created_at"].strftime("%Y-%m-%d %H:%M:%S"),
+            "isNew": 1 if task["created_at"] >= threshold else 0
         }
         for task in tasks
     ]
